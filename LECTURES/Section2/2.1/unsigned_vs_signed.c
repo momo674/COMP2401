09895c9369326a209728bb31d8cacc73836934a6
Mohamad Radaideh <117695265+momo674@users.noreply.github.com>
2023-10-11 08:26:33 -0400

    work

---
+++ LECTURES/Section2/2.1/unsigned_vs_signed.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    unsigned char  value1 = -3;
+    signed char value2 = -3;
+    
+
+
+    printf(" Value1 = %d Value2 = %d ", value1,value2);
+    
+    
+    return 0;
+}
