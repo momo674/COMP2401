b082d8dc1981b4b39527d09d963a81631ba2a0a4
Mohamad Radaideh <mohparrot@gmail.com>
2023-11-06 13:16:19 -0500

    learning double pointers

--- LECTURES/Section3/3.2/p4-dPtr.c
+++ LECTURES/Section3/3.2/p4-dPtr.c
@@ -6,26 +6,39 @@ void doInitGood(int*);
 
 void doAllocBad(int*);
 void doAllocGood(int**);
-
+void pointer(int**, int*);
 
 int main()
 {
   int  num;
   int *ptr;
 
-  doInitBad(num);
-  printf("%d\n", num);
+//   doInitBad(num);
+//   printf("%d\n", num);
 
-  doInitGood(&num);
-  printf("%d\n", num);
+//   doInitGood(&num);
+//   printf("%d\n", num);
+//   doAllocBad(ptr);
+// //  printf("%d\n", *ptr);
 
-  doAllocBad(ptr);
-//  printf("%d\n", *ptr);
+//   doAllocGood(&ptr);
+//   printf("%d\n", *ptr);
 
-  doAllocGood(&ptr);
-  printf("%d\n", *ptr);
+  int x = 1;
 
-  return 0;
+  int* p = &x;
+  int* p2 = &x;
+  
+  int* po;
+  pointer(&po, &x);
+  printf("Pointer points to: %d\n", *po);
+    return 0;
+
+}
+
+void pointer(int** p, int* x) {
+  *p = x;
+  **p += 10;
 }
 
 void doAllocGood(int **p)
@@ -36,8 +49,11 @@ void doAllocGood(int **p)
 
 void doAllocBad(int *p)
 {
-  p = malloc(sizeof(int));
+  p = malloc(sizeof(int)); //not modifiying orignial pointer
   *p = 80;
+  //POINTER WILL NOW DEREFENECE TO 80; HOWEVER ORIGINAL POINTER WILL NOT POINT TO
+  //BLOCK OF MEMORY.
+  //p = malloc(sizeof(int)) basically
 }
 
 void doInitGood(int *n)
@@ -54,4 +70,26 @@ void doInitBad(int n)
 
 
 
+//when you pass a pointer by value, any changes to the pointer (where it points to) will not effect original pointer.
+//ex:
+/*
+  void pass(int* x) {
+    x = &GLOBAL_INT_VARIABLE
+  }
+*/
 
+
+/*
+  if we have a function, which sets the pointer to derefence at a value, we can pass by value
+  if we have a function where pointer is pointing malloc address, we must use double pointer
+    -> if we dont, this will happen
+
+    int* pointer;
+    init(pointer);
+    printf(*pointer) will be NULL. The heap has a block of 10 bytes not used aswell.
+    
+    void init(*p){
+      p = malloc(10);
+    }
+
+*/
